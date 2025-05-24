e20d8e64e211d4649ee72c447a12d37a11a19d9c
anna23q <anblinda4@gmail.com>
2025-05-24 16:50:14 +0300

    Complete hw-01 with all functions tested

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-const message = `hello Petya`;
+const message = hello Petya;
 
 console.log(message);
 
-const bobik = `Hi is a dog. Have you got ${5} eye?`;
+const bobik = Hi is a dog. Have you got ${5} eye?;
 
 console.log(bobik);
